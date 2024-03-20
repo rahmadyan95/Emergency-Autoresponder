766bd71b7f02072f699882dc14b4310158b22141
Rahmadyan <haekal.rahmadyan95@gmail.com>
2024-03-20 16:28:33 +0700

    add dashboard

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ class tkinterApp (ctk.CTk):
 class Login_page(ctk.CTkFrame):
     def __init__(self, parent,controller):
         super().__init__(parent)
+        self.controller = controller
         self._set_appearance_mode("Light")
         self.script_dir = os.path.dirname(os.path.abspath(__file__))
         
@@ -46,7 +47,7 @@ class Login_page(ctk.CTkFrame):
     def run(self):
         self.login_page()
 
-    def toggle_first_run(self):
+    def toggle_first_run(self, event=None):
         self.first_run = not self.first_run
         self.login_page()
 
@@ -88,9 +89,9 @@ class Login_page(ctk.CTkFrame):
             self.login_register_title.configure(text="Sign In Responder")
             self.login_register_title1.configure(text="Badge ID")
 
-            self.input_badge_id = CTkEntry(login_register_box,height=50,width=400, fg_color="white",font=self.font_not_bold(18),
+            self.input_badge_id_signin = CTkEntry(login_register_box,height=50,width=400, fg_color="white",font=self.font_not_bold(18),
                                       text_color="grey20",border_width=0,placeholder_text="")
-            self.input_badge_id.place(x=30,y=120)
+            self.input_badge_id_signin.place(x=30,y=120)
 
             self.login_register_title1 = CTkLabel(login_register_box,text="Password",font=self.font(18),text_color="grey20")
             self.login_register_title1.place(x=30,y=180)
@@ -106,10 +107,8 @@ class Login_page(ctk.CTkFrame):
             self.check_box.place(relx=0.9,rely=0.5,anchor=W)
 
             
-
-            
             button_login = CTkButton(login_register_box, text="Login",fg_color='#2B70C9',font=self.font(18),hover_color='grey30', 
-                                        command=self.toggle_first_run,height=50,width=400)
+                                        command=  lambda: self.controller.show_frame(StartPage),height=50,width=400)
             button_login.place(relx=0.5, rely=0.58, anchor=CENTER)
 
             self.long_line(login_register_box,170,3,30,385)
@@ -123,10 +122,51 @@ class Login_page(ctk.CTkFrame):
 
        
         elif self.first_run == False :
-            login_field_title = CTkLabel(login_field,text="–✦– Register –✦–",font=self.font(48))
-            login_field_title.place(relx=0.5,rely=0.05,anchor=N)
-            button_signin = CTkButton(login_field, text="Switch to Sign In", command=self.toggle_first_run)
-            button_signin.place(relx=0.5, rely=0.1, anchor=N)
+
+            self.login_register_title.configure(text="Register Responder")
+            self.login_register_title1.configure(text="Responder Name")
+
+            self.input_responder_name_register = CTkEntry(login_register_box,height=50,width=400, fg_color="white",font=self.font_not_bold(18),
+                                      text_color="grey20",border_width=0,placeholder_text="")
+            self.input_responder_name_register.place(x=30,y=120)
+
+            self.login_register_title1 = CTkLabel(login_register_box,text="Badge ID",font=self.font(18),text_color="grey20")
+            self.login_register_title1.place(x=30,y=180)
+
+            self.input_badge_id_register = CTkEntry(login_register_box,height=50,width=400, fg_color="white",font=self.font_not_bold(18),
+                                      text_color="grey20",border_width=0,placeholder_text="")
+            self.input_badge_id_register.place(x=30,y=215)
+
+            self.login_register_title1 = CTkLabel(login_register_box,text="Password",font=self.font(18),text_color="grey20")
+            self.login_register_title1.place(x=30,y=280)
+
+            self.input_badge_password_register = CTkEntry(login_register_box,height=50,width=400, fg_color="white",font=self.font_not_bold(18),
+                                      text_color="grey20",border_width=0,placeholder_text="",show='*')
+            self.input_badge_password_register.place(x=30,y=310)
+
+            self.check_box = CTkCheckBox(self.input_badge_password_register,height=15,width=15,text="",text_color="grey20",
+                                         font=self.font(12),onvalue=0,offvalue=1,command=self.show_password,checkbox_height=30,
+                                         checkbox_width=30)
+            self.check_box.place(relx=0.9,rely=0.5,anchor=W)
+
+            self.submit_register = CTkButton(login_register_box,height=50,width=400,fg_color='#42b72a',text_color="white",
+                                             font=self.font(18),text='Submit')
+            self.submit_register.place(x=30,y=380)
+
+            self.long_line(login_register_box,170,3,30,460)
+            self.text(login_register_box,"or",self.font(20),"grey20",215,446)
+            self.long_line(login_register_box,170,3,250,460)
+
+            have_account_label = CTkLabel(login_register_box,text="Have account?",font= (self.font_not_bold(16)),
+                                          cursor='hand2',text_color='grey10')
+            have_account_label.place(relx=0.5 ,rely=0.94, anchor=S)
+            have_account_label.bind('<Button-1>',self.toggle_first_run)
+            
+            
+            # login_field_title = CTkLabel(login_field,text="–✦– Register –✦–",font=self.font(48))
+            # login_field_title.place(relx=0.5,rely=0.05,anchor=N)
+            # button_signin = CTkButton(login_field, text="Switch to Sign In", command=self.toggle_first_run)
+            # button_signin.place(relx=0.5, rely=0.1, anchor=N)
 
     def text(self,position,teks,font,color,x,y):
         text = CTkLabel(position,text=teks,font=font,text_color=color)
@@ -142,9 +182,12 @@ class Login_page(ctk.CTkFrame):
     def show_password(self):
         if self.check_box.get() == 1 :
             self.input_password_id.configure(show="*")
+            self.input_badge_password_register.configure(show="*")
+            
         
         elif self.check_box.get() == 0 :
             self.input_password_id.configure(show="")
+            self.input_badge_password_register.configure(show="")
 
     
 
@@ -161,14 +204,94 @@ class StartPage(ctk.CTkFrame):
     def __init__(self, parent,controller):
         super().__init__(parent)
         self._set_appearance_mode("Light")
+        self.script_dir = os.path.dirname(os.path.abspath(__file__))
 
-        label = CTkLabel(self,text="Hello")
-        label.place(x=10,y=10)
+        self.dashboard()
+        self.sidebar()
+        self.responder_status()
+        self.notification_field()
+
+
+    def dashboard(self) :
+        self.box = CTkFrame(self,height=788, width=1400,fg_color="#E5E4E2",bg_color="white")
+        self.box.place(x=0,y=0)
+
+        dashboard_title = CTkLabel(self.box,text='Main Dashboard',text_color="grey20",font=('Arial Bold',22))
+        dashboard_title.place(relx=0.34, rely= 0.04, anchor=NE)
+
+        dashboard_title_1 = CTkLabel(self.box,text='Service Menu',text_color="grey20",font=('Arial Bold',20))
+        dashboard_title_1.place(relx=0.31, rely= 0.09, anchor=NE)
+
+        self.search = CTkEntry(self.box,height=30,width=200, fg_color="white",font=('Arial',16),
+                                      text_color="grey20",border_width=0,placeholder_text="Search",border_color="black")
+        self.search.place(relx=0.91,rely=0.02,anchor=NE)
+
+    def responder_status(self):
+        title = CTkLabel(self.box,height=35,width=300,fg_color='#642424',text='',corner_radius=5)
+        title.place(relx=0.982,rely=0.075,anchor=NE)
+
+        title_1 = CTkLabel(title,text='Responder Profiles',text_color='white',font=('Arial Bold',14))
+        title_1.place(relx=0.5,rely=0.5,anchor=E)
+
+        responder_profile_box = CTkLabel(self.box,height=160,width=300,fg_color='white',text='',corner_radius=5)
+        responder_profile_box.place(relx=0.768,rely=0.23,anchor=W)
+
+        name = CTkLabel(responder_profile_box,text="Name",font=('Arial',14),
+                             text_color='grey40')
+        name.place(x=150, y = 15)
+
+        self.inputname = CTkLabel(responder_profile_box,text="Umam Madura",font=('Arial Bold',16),
+                             text_color='grey10')
+        self.inputname.place(x=150, y = 35)
+
+        name = CTkLabel(responder_profile_box,text="Division",font=('Arial',14),
+                             text_color='grey40')
+        name.place(x=150, y = 60)
+
+        self.input_divison = CTkLabel(responder_profile_box,text="69 Division",font=('Arial Bold',16),
+                             text_color='grey10')
+        self.input_divison.place(x=150, y = 83)
+
+        
+
+
+
+
+    def notification_field(self):
+        notification_title = CTkLabel(self.box,text='Accident Notification',text_color="grey20",font=('Arial Bold',20))
+        notification_title.place(relx=0.91,rely=0.35,anchor=NE)
+
+        AER_box = CTkLabel(self.box,height=250,width=300,fg_color='#642424',text='',corner_radius=5)
+        AER_box.place(relx=0.768,rely=0.55,anchor=W)
+
+        
+        
+
+
+
+
+
+
+    def sidebar(self): 
+        sidebar_bg = CTkLabel(self.box,height=790,width=300,fg_color='#642424',corner_radius=18,text='')
+        sidebar_bg.place(relx=0.19,rely=0.5,anchor=E)
+
+        logo_directory = os.path.join(self.script_dir, 'assets', 'App_logo.png')
+        logo = CTkImage(light_image=Image.open(logo_directory), size=(230,95))
+        logo_placeholder = CTkButton(sidebar_bg,image=logo,fg_color='transparent',text='',state="diabled" ,border_width=0,border_color="white")
+        logo_placeholder.place(relx=0.55,rely=0.03,anchor=N)
+
+
+
+
+
 
 class Page1(ctk.CTkFrame):
     def __init__(self, parent,controller):
         super().__init__(parent)
         self._set_appearance_mode("Light")
+        
+    
 
 class Page2(ctk.CTkFrame):
     def __init__(self, parent,controller):
